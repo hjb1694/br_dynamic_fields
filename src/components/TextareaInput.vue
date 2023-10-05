0ec1a1a2010ba1504c7ac69e688295aa770b8efe
Hayden Bradfield <hbradfield20@gmail.com>
2023-10-05 15:32:32 -0400

    style textarea

--- src/components/TextareaInput.vue
+++ src/components/TextareaInput.vue
@@ -5,6 +5,7 @@
         :value="modelValue" 
         class="textarea-input__field" 
         rows="10"
+        placeholder=" "
         ></textarea>
         <label class="textarea-input__label">{{ label }}</label>
     </div>
@@ -29,3 +30,43 @@
         }
     }
 </script>
+
+<style lang="scss" scoped>
+    .textarea-input {
+        position:relative;
+        background:#fff;
+        height:16rem;
+        border:1px solid #bbb;
+        border-radius:5px;
+        overflow: hidden;
+
+        &__field {
+            height:14.5rem;
+            position:absolute;
+            bottom:0;
+            left:0;
+            width:100%;
+            outline:none;
+            border:none;
+            padding:4px;
+            font-family: inherit;
+            font-size:1.6rem;
+            resize:none;
+        }
+
+        &__label {
+            position:absolute;
+            top:1.4rem;
+            left:4px;
+            pointer-events: none;
+            font-size:1.8rem;
+            transition:all .3s;
+        }
+
+        &__field:focus + .textarea-input__label, 
+        &__field:not(:placeholder-shown) + .textarea-input__label {
+            font-size:1.3rem;
+            transform:translateY(-1rem);
+        }
+    }
+</style>
